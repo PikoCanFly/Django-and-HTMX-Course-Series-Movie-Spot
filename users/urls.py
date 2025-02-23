44461c52efddf612735be2392e948de71a023ee6
unknown <pikocanfly@gmail.com>
2025-02-23 04:39:37 +0100

    video-8

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import Login, RegisterUser, Logout, profile, create_list
+from .views import Login, RegisterUser, Logout, profile, create_list, delete_list
 
 urlpatterns = [
     path("login/", Login.as_view(), name="login"),
     path("register/", RegisterUser.as_view(), name="register"),
     path("logout/", Logout.as_view(), name="logout"),
     path("profile/", profile, name="profile"),
-    path("create_list/", create_list, name="create_list")
+    path("create_list/", create_list, name="create_list"),
+    path("delete_list/<int:list_id>", delete_list, name="delete_list")
 ]
--- users/views.py
+++ users/views.py
@@ -5,7 +5,8 @@ from .forms import RegisterUserForm, CreateListForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from .models import UserList
-
+from django.shortcuts import get_object_or_404
+from django.http.response import HttpResponseForbidden
 
 class Login(LoginView):
     template_name = "users/accounts/login.html"
@@ -32,6 +33,7 @@ def profile(request):
     return render(request, "users/accounts/profile.html", {"user":user,
                                                            "user_lists":user_lists})
 
+@login_required
 def create_list(request):
     user = request.user
     if request.method == "POST":
@@ -52,3 +54,15 @@ def create_list(request):
         
     form = CreateListForm()
     return render(request, "users/lists/partials/_create_list_form.html", {"form":form})
+
+@login_required
+def delete_list(request, list_id):
+    user_list = get_object_or_404(UserList, id=list_id, user=request.user)
+    if request.method == "POST":
+        user_list.delete()
+        user_lists = UserList.objects.filter(user= request.user)
+        return render(request, "users/lists/partials/_user_lists.html",
+                          {"user_lists":user_lists})
+    return HttpResponseForbidden
+    
+    
